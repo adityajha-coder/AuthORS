49f50c71d18fb4b8dbf2c2502ae387118535daf2
Aditya Jha <adityajha97172@gmail.com>
2026-08-27 23:04:26 +0530

    auth tokens init

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -28,17 +28,98 @@ export async function register(req, res){
         password: hashedPassword
     });
 
-    const token = jwt.sign({
+    const accessToken = jwt.sign({
         id: user._id,
     }, config.JWT_SECRET, {
-        expiresIn: "1d"
+        expiresIn: "15m"
+    })
+    
+    const refreshToken = jwt.sign({
+        id: user._id,
+    }, config.JWT_SECRET, {
+        expiresIn: "7d"
     })
 
-    res
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+    })
+
+    return res
     .status(201)
     .json({
         message: "User registered successfully",
-        token
+        user: {
+            username: user.username,
+            email: user.email,
+        },
+        accessToken,
     });
 
+}
+
+export async function getMe(req, res){
+    const token = req.headers.authorization?.split(" ")[ 1 ];
+
+    if(!token){
+        return res
+        .status(401)
+        .json({message: "Unauthorized"});
+    }
+
+    const decoded = jwt.verify(token, config.JWT_SECRET);
+
+    const user = await userModel.findById(decoded.id)
+
+    res
+    .status(200)
+    .json({
+        message: "User fetched successfully",
+        user: {
+            userName: user.userName,
+            email: user.email,
+        }
+    })
+}
+
+export async function refreshToken(req, res){
+    const refreshToken = req.cookies.refreshToken;
+
+    if(!refreshToken){
+        return res.status(401).json({
+            message: "Refresh token not found"
+        })
+    }
+
+    const decoded = jwt.verify(refreshToken. config.JWT_SECRET)
+
+    const accessToken = jwt.sign({
+        id: decoded.id
+    }, config.JWT_SECRET,
+        {
+            expiresIn: "15m"
+        }
+    )
+
+    const newRefreshToken = jwt.sign({
+        id: decoded.id
+    }, config.JWT_SECRET,
+        {
+            expiresIn: "7d"
+        }
+    )
+
+    res.cookie("refreshToken", newRefreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "Strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+    })
+
+    res.status(200).json({
+        message: "Access token rfreshed sucessfully",
+        accessToken
+    })
 }
